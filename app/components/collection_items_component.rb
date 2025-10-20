d1a86d6020688e48d71f3d6febee4a305fe18fa9
Brittny Lapierre <brittny.lapierre@gmail.com>
2025-10-20 10:54:08 -0400

    Adding back changes from Demo to Base (#14)
    * environment chabges
    
    * saving config
    
    * Setup
    
    * Hierarchy working
    
    * Adding breadcrumbs
    
    * Cleaner paths
    
    * Fixed facets
    
    * breadcrumb fix
    
    * Show title in breadcrumb
    
    * Add sort to collection items
    
    * Fix redirect
    
    * Add ark check
    
    * fix for missing ark
    
    * improve item listing

--- app/components/collection_items_component.rb
+++ app/components/collection_items_component.rb
@@ -4,14 +4,19 @@ class CollectionItemsComponent < ViewComponent::Base
     @page = page.to_i
     @per_page = per_page.to_i
 
-    rsolr = RSolr.connect url: 'http://public:hdwi389e8d!ds@4.229.225.26/solr/blacklight_marc'
+    rsolr = RSolr.connect url: 'http://public:hdwi389e8d!ds@4.229.225.26/solr/blacklight_marc_demo'
 
     start = (@page - 1) * @per_page
 
     @response_data = rsolr.get 'select', params: {
-      q: "serial_key:#{@documentId}",
+      q: '*:*',
+      fq: [
+        "serial_key:#{RSolr.solr_escape(@documentId)}",
+        'is_issue:Yes'
+      ],
       start: start,
-      rows: @per_page
+      rows: @per_page,
+      sort: 'id asc'
     }
 
     @collection_items = @response_data['response']['docs']

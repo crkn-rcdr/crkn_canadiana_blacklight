dcaafaef2bd12f43539fbd444cba16d1a38fe94e
Brittny Lapierre <brittny.lapierre@gmail.com>
2026-06-03 11:23:51 -0400

    CRKN styling for home page

--- config/initializers/canadiana_endpoints.rb
+++ config/initializers/canadiana_endpoints.rb
@@ -1,5 +1,5 @@
 # Centralize configurable external endpoints and read from ENV.
-Rails.configuration.x.iiif_manifest_base = ENV.fetch('IIIF_MANIFEST_BASE', 'https://crkn-iiif-api.azurewebsites.net/manifest')
+Rails.configuration.x.iiif_manifest_base = ENV.fetch('IIIF_MANIFEST_BASE', 'https://www-iiif-pres.canadiana.ca/manifest')
 Rails.configuration.x.iiif_content_search_base = ENV.fetch('IIIF_CONTENT_SEARCH_BASE', 'https://crkn-iiif-content-search.azurewebsites.net/search')
 Rails.configuration.x.cap_pass = ENV.fetch('CAP_PASS', '')
 
